6e5d00d7db6e58ae7672afb3ad073e94203da03b
陈尚 <chenshang02@meituan.com>
2019-02-19 18:59:51 +0800

    update

--- Log/BaseLog.sh
+++ Log/BaseLog.sh
@@ -5,19 +5,21 @@ source ./../../BaseShell/Date/BaseLocalDate.sh
 source ./../../BaseShell/Date/BaseLocalDateTime.sh
 
 LOG_DIR="${HOME}/.baseshell"
-if [[ ! -d ${LOG_DIR} ]]; then
-  mkdir -p "${LOG_DIR}";
-fi
-LOG_TRACE_MODEL=${TRUE}
+if [[ ! -d ${LOG_DIR} ]]; then mkdir -p "${LOG_DIR}" ;fi
+LOG_TRACE_MODEL="${TRUE}"
+LOG_DEBUG_MODEL="${FALSE}"
 
 ######################################################################
 # 注意 1>&2 每一个日志输入都把标准输出重定向到了标准错误输出,目的是在使用log_的时候不影响函数的返回结果
 
+# 默认关闭,debug级别的日志会忽略
 # debug级别的日志 []<-(msg:String)
 function log_debug(){
-  echo -e "[$(localdatetime_now)][$$ $BASHPID] [DEBUG]:   $*" 1>&2 \
- | tee -a "${LOG_DIR}/$(localdate_now).debug.log" \
- | tee -a "${LOG_DIR}/$(localdate_now).log"
+  if [[ ${LOG_DEBUG_MODEL} -eq ${TRUE} ]];then
+    echo -e "[$(localdatetime_now)][$$ $BASHPID] [DEBUG]:   $*" 1>&2 \
+   | tee -a "${LOG_DIR}/$(localdate_now).debug.log" \
+   | tee -a "${LOG_DIR}/$(localdate_now).log"
+  fi
 }
 
 # info级别的日志 []<-(msg:String)
@@ -48,7 +50,8 @@ function log_success(){
  | tee -a "${LOG_DIR}/$(localdate_now).log"
 }
 
-# 用来标识失败状态的,当前子进程会退出,用红色, []<-(msg:String)
+# @attention 当前子进程会退出
+# 用来标识失败状态的,用红色, []<-(msg:String)
 function log_fail(){
   echo -e "[$(localdatetime_now)][$$ $BASHPID]\\033[31m [FAIL]\\033[0m:    $*" 1>&2 \
  | tee -a "${LOG_DIR}/$(localdate_now).info.log" \
@@ -56,9 +59,10 @@ function log_fail(){
  exit
 }
 
-# 用来追踪日志,日志输出到文件中,不会影响返回值,默认关闭[]<-(msg:String)
+# @attention 日志只会输出到日志文件中,不会输出在控制台上,默认开启
+# 用来标识追踪日志 []<-(msg:String)
 function log_trace(){
-  if [[ ${LOG_TRACE_MODEL} ]];then
-    echo -e "[$(localdatetime_now)][$$ $BASHPID] [TRACE]:   $*" >>${LOG_DIR}/$(localdate_now).trace.log 2>&1
+  if [[ ${LOG_TRACE_MODEL} -eq ${TRUE} ]];then
+    echo -e "[$(localdatetime_now)][$$ $BASHPID] [TRACE]:   $*" >>${LOG_DIR}/"$(localdate_now)".trace.log 2>&1
   fi
 }
