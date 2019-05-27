c9c7581688a462c2050bcc332d6168d6d64b495a
陈尚 <chenshang02@meituan.com>
2019-05-27 20:47:52 +0800

    注解

--- Annotation/BaseAnnotation.sh
+++ Annotation/BaseAnnotation.sh
@@ -5,28 +5,28 @@ source ./../../BaseShell/Log/BaseLog.sh
 #===============================================================
 
 # 判断传入参数是否为空
-function ANO_NotNull(){
+function _NotNull(){
   local param=$1;local err_msg=$2
   err_msg=${err_msg:-'parameter can not be null'}
   [[ -z "${param}" ]] && log_fail "${err_msg}" || return "${TRUE}"
 }
 
 # 判断传入参数是否为数字
-function ANO_Numeric(){
+function _Numeric(){
   local param=$1;local err_msg=$2
   err_msg=${err_msg:-'parameter must be numeric'}
   ! grep -q '^[[:digit:]]*$' <<< "${param}" && log_fail "${err_msg}" ||  return "${TRUE}"
 }
 
 # @param $1:最小值 $2:参数值 (min,)
-function ANO_Min(){
+function _Min(){
   local err_msg="min value is $1"
   # $2:参数值 < $1:最小值
   [[ $2 -lt $1 ]] && log_fail "${err_msg}" || return "${TRUE}"
 }
 
 # @param $1:最小值 $2:参数值 (,max)
-function ANO_Max(){
+function _Max(){
   local err_msg="max value is $1"
   # $2:参数值 > $1:最小值
   [[ $2 -gt $1 ]] && log_fail "${err_msg}" || return "${TRUE}"
