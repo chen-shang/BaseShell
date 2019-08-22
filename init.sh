d2cfb1fc1ac40db2f78b5a694ba18d53e3ef6cc4
陈尚 <chenshang02@meituan.com>
2019-08-22 20:14:15 +0800

    init

--- init.sh
+++ init.sh
@@ -1,41 +1,61 @@
 #!/usr/bin/env bash
 set -u
-read -p "project:" project
+
+read -p "project[项目目录]:" project
 if [[ -z "${project}"  ]];then
   echo "project can not be null"
   exit
 fi
-read -p "module:" module
+
+read -p "module[模块名称]:" module
 if [[ -z "${module}"  ]];then
-  echo "project can not be null"
+  echo "module can not be null"
   exit
 fi
 
 # 新建项目目录
-mkdir ${project}
-cd  ${project}
+mkdir "${project}"
+cd  "${project}"
 
 # 引入BaseShell
 BASE_SHELL=$(dirname ${BASH_SOURCE[0]})
-ln -s ${BASE_SHELL} ./BaseShell
+ln -s "${BASE_SHELL}" ./BaseShell
 
 # 新建模块
-mkdir ${module}
+mkdir "${module}"
+cd "${module}"
 
-cd ${module}
-mkdir Resources
-mkdir Service
-mkdir Test
+mkdir Resources Service Test
 
+# 写入默认的配置文件
 echo "#!/usr/bin/env bash
 # shellcheck disable=SC1091,SC2155
 #===============================================================
 # 是否显示BANNER
-SHOW_BANNER=1
+SHOW_BANNER=0
 # 日志级别
 LOG_LEVEL=DEBUG
 " > config.sh
 
-echo "# ${module}
-" > readme.md
-tree ./../../${project}
+# 写入默认的readme文件
+echo "# ${module}" > readme.md
+
+echo "#!/usr/bin/env bash
+# shellcheck disable=SC1091,SC2155
+#===============================================================
+import=\$(basename \"\${BASH_SOURCE[0]}\" .sh)
+if [[ \$(eval echo '$'\"\${import}\") == 0 ]]; then return; fi
+eval \"\${import}=0\"
+#===============================================================
+source ../../BaseShell/Starter/BaseHeader.sh
+#导入工具包
+#===============================================================================
+#业务代码
+main(){
+  manual
+}
+#===============================================================================
+source ../../BaseShell/Starter/BaseEnd.sh
+" > "./Service/${module}Service.sh"
+
+tree "./../../${project}"
