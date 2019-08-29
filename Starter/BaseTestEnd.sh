db586791379c4f41e72259552137077c8bb316b1
陈尚 <chenshang02@meituan.com>
2019-08-29 21:42:02 +0800

    list map

--- Starter/BaseTestEnd.sh
+++ Starter/BaseTestEnd.sh
@@ -4,47 +4,32 @@ source ./../../BaseShell/Starter/BaseStarter.sh
 
 # 断言目标值是
 assertEquals(){
-  $(equals "$1" "$2") && {
-    log_success "test ok[100%],hit [${2}]"
-  }||{
-    log_error "test fail[100%],expect [${2}] but [${1}]"
-  }
+  equals "$1" "$2" && log_success "test ok[100%],hit [${2}]"
+  equals "$1" "$2" || log_error "test fail[100%],expect [${2}] but [${1}]"
 }
 
 # 断言目标值不为空
 assertNotNull(){
-  $(isNotBlank "$1") && {
-     log_success "test ok[100%],hit [${1}]"
-   }||{
-     log_error "test fail[100%],expect not null"
-   }
+  isNotBlank "$1" && log_success "test ok[100%],hit [${1}]"
+  isNotBlank "$1" || log_error "test fail[100%],expect not null"
 }
 
 # 断言目标值为空
 assertNull(){
-  $(isBlank "$1") && {
-    log_success "test ok[100%],hit [${1}]"
-  }||{
-    log_error "test fail[100%],expect null but [${1}]"
-  }
+  isBlank "$1" && log_success "test ok[100%],hit [${1}]"
+  isBlank "$1" || log_error "test fail[100%],expect null but [${1}]"
 }
 
 # 断言目标值为假(非零)
 assertFalse(){
-  $(equals "$1" "${FALSE}") && {
-    log_success "test ok[100%],hit [FALSE]"
-  }||{
-    log_error "test fail[100%],expect false [FALSE] but [TRUE]"
-  }
+  equals "$1" "${FALSE}" && log_success "test ok[100%],hit [FALSE]"
+  equals "$1" "${FALSE}" || log_error "test fail[100%],expect false [FALSE] but [TRUE]"
 }
 
 # 断言目标值为真(为零)
 assertTrue(){
-  $(equals "$1" "${TRUE}") && {
-    log_success "test ok[100%],hit [TRUE]"
-  }||{
-    log_error "test fail[100%],expect [TRUE] but [FALSE]"
-  }
+  equals "$1" "${TRUE}" && log_success "test ok[100%],hit [TRUE]"
+  equals "$1" "${TRUE}" || log_error "test fail[100%],expect [TRUE] but [FALSE]"
 }
 
 # 测试函数执行函数
