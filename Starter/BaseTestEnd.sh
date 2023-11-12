ec114a51de46fe3fc5db71afd085a80f50d34dd7
chenxiaoshang <chenshang@chenshangdeMacBook-Pro.local>
2023-11-12 11:24:04 +0800

    fix

--- Starter/BaseTestEnd.sh
+++ Starter/BaseTestEnd.sh
@@ -7,7 +7,7 @@ source ./../../BaseShell/Starter/BaseImported.sh && return
 # 取出测试文件中待测试的方法
 # test-开头的方法
 # 不被#ignore注释的
-methodList=$(cat <"$0" | grep "test-" |grep -v "grep" |grep -E "(){|() {"| grep -v "#ignore" | sed "s/(){//g")
+methodList=$(cat <"$0" | grep "test-" | grep -v "grep" | grep -E "(){|() {" | grep -v "#ignore" | sed "s/(){//g" | sed "s/() {//g")
 # 测试文件名
 testFile=$(basename "${BASH_SOURCE[1]}" .sh)
 for method in ${methodList}; do
