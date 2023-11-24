f0a320c5ccaf201bb727019df39c1a6823251e4f
chenxiaoshang <chenshang@chenshangdeMacBook-Pro.local>
2023-11-24 23:59:48 +0800

    fix

--- Json/BaseJson.sh
+++ Json/BaseJson.sh
@@ -5,52 +5,63 @@
 source ./../../BaseShell/Starter/BaseImported.sh && return
 source ./../../BaseShell/Starter/BaseStarter.sh
 #===============================================================================
-function json_toString(){
+function json_toString() {
   local param=$*
-  _action(){
+  _action() {
     local json=$1
     #  --compact-output/ -c：
     #默认情况下，jq pretty-prints JSON输出。通过将每个JSON对象放在一行上，使用此选项将导致输出更紧凑。
-    echo "${json}"|jq -c .
+    echo "${json}" | jq -c .
   }
   pip "${param}"
 }
 
-function json_toPrettyString(){
+function json_toPrettyString() {
   local param=$*
-  _action(){
+  _action() {
     local json=$1
-    echo "${json}"|jq .
+    echo "${json}" | jq .
   }
   pip "${param}"
 }
 
-function json_empty(){
+function json_empty() {
   local keys=$*
   local json="{}"
-  for k in ${keys};do
-    json=$(echo "${json}"|jq .${k}=\"\")
+  for k in ${keys}; do
+    json=$(echo "${json}" | jq .${k}=\"\")
   done
   echo "${json}"
 }
 
-function json_new(){
+function json_new() {
   local criteria="$1"
   local json="{}"
-  local kvs=$(echo "${criteria}"|awk -F '&' '{for(i=1;i<=NF;i++){print $i}}')
+  local kvs=$(echo "${criteria}" | awk -F '&' '{for(i=1;i<=NF;i++){print $i}}')
   local end="====end===="
-  echo -e "${kvs}\n${end}"|while read kv;do
+  echo -e "${kvs}\n${end}" | while read kv; do
     equals "${kv}" "${end}" && echo "${json}"
-    local k="$(echo "${kv}"|awk -F '=' '{print $1}')"
-    local v="$(echo "${kv}"|awk -F '=' '{print $2}')"
-    local type=$(echo ${v}|jq -r 'type' 2>/dev/null )
+    local k="$(echo "${kv}" | awk -F '=' '{print $1}')"
+    local v="$(echo "${kv}" | awk -F '=' '{print $2}')"
+    local type=$(echo ${v} | jq -r 'type' 2>/dev/null)
     case "${type}" in
-      "number"|"object"|"array")
-        json=$(echo "${json}"|jq -c .${k}="${v}")
+    "number" | "object" | "array")
+      json=$(echo "${json}" | jq -c .${k}="${v}")
       ;;
-      *)
-        json=$(echo "${json}"|jq -c ".${k}=\"${v}\"")
+    *)
+      json=$(echo "${json}" | jq -c ".${k}=\"${v}\"")
       ;;
     esac
   done
+}
+
+function json_toCvs() {
+  local param=$*
+  _action() {
+    local json=$1
+    #  --compact-output/ -c：
+    #默认情况下，jq pretty-prints JSON输出。通过将每个JSON对象放在一行上，使用此选项将导致输出更紧凑。
+    echo "${json}" | jq -r '(.[0] | keys_unsorted) as $keys | $keys, map([.[ $keys[] ]])[] | @csv' | sed 's/"//g'
+  }
+  pip "${param}"
 }
--- Test/BaseJsonTest.sh
+++ Test/BaseJsonTest.sh
@@ -6,18 +6,29 @@ source ./../../BaseShell/Starter/BaseHeader.sh
 source ./../../BaseShell/Json/BaseJson.sh
 #===============================================================
 
-test-json_empty(){
+test-json_empty() {
   local json=$(json_empty)
   assertEquals "${json}" "{}"
 }
 
-test-json_new(){
+test-json_new() {
   local user1=$(json_new "id=1&name=chenshang")
-  echo "${user1}"|json_toString
+  echo "${user1}" | json_toString
   local user2=$(json_new "id=2&name=chenshang22")
-  echo "${user2}"|json_toPrettyString
+  echo "${user2}" | json_toPrettyString
   local json=$(json_new "user=[${user1},${user2}]")
-  echo "${json}"|jq .
+  echo "${json}" | jq .
+}
+
+test-json_toCvs() {
+  json='[{"name":"Alice","age":25,"location":"City A"},{"name":"Bob","age":30,"location":"City B"}]'
+  local result=$(echo "${json}" | json_toCvs)
+  local line0=$(echo "${result}" | awk 'NR==1')
+  local line1=$(echo "${result}" | awk 'NR==2')
+  local line2=$(echo "${result}" | awk 'NR==3')
+  assertEquals "name,age,location" "${line0}"
+  assertEquals "Alice,25,City A" "${line1}"
+  assertEquals "Bob,30,City B" "${line2}"
 }
 #===============================================================
 source ./../../BaseShell/Starter/BaseTestEnd.sh
